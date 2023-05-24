3ad13c41a961a792cb26514b78d9c4cfabc81d39
Катасонова Мария <katasonova_masa@mail.ru>
2023-05-24 17:03:37 +0300

    true

--- src/test/java/tests/mobile/TestBase.java
+++ src/test/java/tests/mobile/TestBase.java
@@ -14,38 +14,36 @@ import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static io.qameta.allure.Allure.step;
 
 public class TestBase {
-    static String deviceHost = System.getProperty("deviceHost");
-    @BeforeAll
-    public static void setup() {
-        addListener("AllureSelenide", new AllureSelenide());
-        switch (deviceHost) {
-            case "browserstack":
-                Configuration.browser = BrowserstackDriver.class.getName();
-                break;
-            case "local":
-                Configuration.browser = LocalDriver.class.getName();
-                break;
-            default:
-                throw new RuntimeException();
+        static String deviceHost = System.getProperty("deviceHost");
+        @BeforeAll
+        public static void setup() {
+            addListener("AllureSelenide", new AllureSelenide());
+            switch (deviceHost) {
+                case "browserstack":
+                    Configuration.browser = BrowserstackDriver.class.getName();
+                    break;
+                case "local":
+                    Configuration.browser = LocalDriver.class.getName();
+                    break;
+                default:
+                    throw new RuntimeException();
+            }
+            Configuration.browserSize = null;
         }
-        Configuration.browserSize = null;
-    }
 
-    @BeforeEach
-    public void startDriver() {
-        addListener("AllureSelenide", new AllureSelenide());
-        open();
-    }
+        @BeforeEach
+        public void startDriver() {
+            addListener("AllureSelenide", new AllureSelenide());
+            open();
+        }
 
-    @AfterEach
-    public void afterEach() {
-        String sessionId = sessionId().toString();
-        //  Attach.screenshotAs("Last screenshot");
-        // Attach.pageSource();
-        closeWebDriver();
-        Attach.addVideo(sessionId);
-        if (deviceHost.equals("browserstack")) {
+        @AfterEach
+        public void afterEach() {
+            String sessionId = sessionId().toString();
+            closeWebDriver();
             Attach.addVideo(sessionId);
+            if (deviceHost.equals("browserstack")) {
+                Attach.addVideo(sessionId);
+            }
         }
-    }
 }
