1da8ded86882795cbd5ee31c4c8db2ca2e1b841f
Катасонова Мария <katasonova_masa@mail.ru>
2023-05-29 21:32:42 +0300

    true

--- src/test/java/drivers/BrowserstackDriver.java
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -25,7 +25,6 @@ public class BrowserstackDriver implements WebDriverProvider {
         MutableCapabilities mutableCapabilities = new MutableCapabilities();
         mutableCapabilities.merge(capabilities);
         // Set your access credentials
-          mutableCapabilities.setCapability(MobileCapabilityType.NO_RESET, false);
         mutableCapabilities.setCapability("browserstack.user", config.username());
         mutableCapabilities.setCapability("browserstack.key", config.passwordKey());
         mutableCapabilities.setCapability("app", config.app());
