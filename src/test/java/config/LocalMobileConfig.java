a0c693fde49de4f4f698f8f1a8d02f0602a33fab
Катасонова Мария <katasonova_masa@mail.ru>
2023-06-17 11:55:55 +0300

    final

--- src/test/java/config/LocalMobileConfig.java
+++ src/test/java/config/LocalMobileConfig.java
@@ -3,7 +3,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:${env}.properties"
+        "classpath:test.properties"
 })
 public interface LocalMobileConfig extends Config {
     @Key("deviceName")
--- src/test/java/config/WebConfig.java
+++ src/test/java/config/WebConfig.java
@@ -2,7 +2,7 @@ package config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:${env}.properties"
+        "classpath:test.properties"
 })
 public interface WebConfig extends Config {
     @Config.Key("baseUrl")
